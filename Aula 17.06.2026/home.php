ecca7aa4705311cdde8e709cdedf5573529be9cd
João Pedro Jensen Farias <jp.123pedrofarias@gmail.com>
2026-06-17 17:28:18 -0300

    Aula 17.06.2026

---
+++ Aula 17.06.2026/home.php
@@ -0,0 +1,6 @@
+<?php require_once 'cabecalho.php'; ?>
+<section class="imagem">
+	<img src="img/girl.gif">
+</section>
+</body>
+</html>
---
+++ Aula 17.06.2026/parimpar.php
@@ -0,0 +1,18 @@
+<?php require_once 'cabecalho.php'; ?>
+<form action="parimpar.php" method="GET">
+	<h1>Par ou ímpar</h1>
+	<p>Digite um número</p>
+	<p><input type="number" name="numero" step="1" min="1" required></p>
+	<p><input type="submit" name="botao" value="Verificar"></p>
+</form>
+<?php
+	if (isset($_GET['botao'])) { //teste do envio do formulário 
+		$numero=$_GET['numero'];
+		$numero=number_format($numero,0,"",".");
+		echo "<section class='resultado'>";
+		if ($numero%2==0) {
+			echo "<p>O número $numero é par!</p>";
+		}else{
+			echo "<p>O número $numero é ímpar!</p>";
+		}
+	}
