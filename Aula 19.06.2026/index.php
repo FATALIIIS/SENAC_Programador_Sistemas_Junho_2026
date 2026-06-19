e1d0724ee64a789aec68495f7b93c244ebeb3751
João Pedro Jensen Farias <jp.123pedrofarias@gmail.com>
2026-06-19 17:37:01 -0300

    Aula 19.06.2026

---
+++ Aula 19.06.2026/index.php
@@ -0,0 +1,38 @@
+<?php require_once 'cabecalho.php'; ?>
+<body class="background">
+<section class="topo">
+	<div id="logo">
+		<a href="index.php">
+			<img src="img/icon2.png">
+		</a>
+	</div>
+	<div id="menu">
+		<ul class="nav">
+			<li>Lógica de Programação 1
+				<ol>
+					<li><a href="primeira.php" target="janela">Demonstração</a></li>
+					<li><a href="soma.php" target="janela">Somatória</a></li>
+					<li><a href="subtracao.php" target="janela">Subtração</a></li>
+					<li><a href="divisao.php" target="janela">Divisão</a></li>
+					<li><a href="multiplicacao.php" target="janela">Multiplicação</a></li>
+					<li><a href="potenciacao.php" target="janela">Potenciação</a></li>
+					<li><a href="produtos.php" target="janela">Produtos</a></li>					
+					<li><a href="idade.php" target="janela">Idade</a></li>
+					<li><a href="triangulo.php" target="janela">Triângulo</a></li>
+				</ol>
+			</li>
+			<li>Lógica de Programação 2 (IF)
+				<ol>
+					<li><a href="parimpar.php" target="janela">Par e Ímpar</a></li>
+					<li><a href="livros.php" target="janela">Livros</a></li>
+					<li><a href="imc.php" target="janela">I.M.C</a></li>
+					<li><a href="media.php" target="janela">Média</a></li>
+					<li><a href="escola.php" target="janela">Escola</a></li>
+		</ul>					
+	</div>
+</section>
+<section class="conteudo">
+	<iframe src="home.php" name="janela"></iframe>
+</section>
+</body>
+</html>
